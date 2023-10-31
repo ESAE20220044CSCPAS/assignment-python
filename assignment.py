06ebc0a522c3f54e5ffbd86e8011f1a40602725d
ADEKOYA ADESESAN R <149457201+ESAE20220044CSCPAS@users.noreply.github.com>
2023-10-31 04:45:22 -0700

    assignment.py

--- assignment.py
+++ assignment.py
@@ -7,11 +7,11 @@ Information = {
     "Level" : "200",
     "Gender" : "Male"
 }
-pattern= r"1"
+pattern= r"E"
 matching_value= [value for value in Information.keys() if re.search(pattern,value)]
 replaced_dict = {}
 for key, value in Information.items():
-    replaced_value = re.sub(pattern, "g", value)
+    replaced_value = re.sub(pattern, "G", value)
     replaced_dict[key] = replaced_value
     print (replaced_dict)
 
